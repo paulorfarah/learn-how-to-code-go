6a3d7e85aff1ec754a25a9df625cde86428d8182
Paulo Farah <paulorfarah@gmail.com>
2020-09-24 13:26:12 -0300

    global variables and types

--- 010-short-declaration-operator/main.go
+++ 010-short-declaration-operator/main.go
@@ -2,18 +2,24 @@ package main
 
 import "fmt"
 
-var z = 43
+var y = 43
+var z = "Shaken, not Stirred"
+var a = `James said, "Shaken, not stirred"`
 
 func main() {
 	x := 42
 	fmt.Println(x)
 	x = 99
 	fmt.Println(x)
-	y := 100 + 70
 	fmt.Println(y)
+	fmt.Printf("%T\n", y)
 //	z :=  "Bond, James"
 	fmt.Println(z)
+	fmt.Printf("%T\n", z)
 	foo()
+	fmt.Printf("%T\n", z)
+	fmt.Println(a)
+	fmt.Printf("%T\n", a)
 }
 
 func foo() {
